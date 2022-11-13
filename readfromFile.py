7e33d6043ee1078a2588aa98f4d397d76b03ec21
anveshreddy725 <54026990+anveshreddy725@users.noreply.github.com>
2022-11-13 01:21:32 -0700

    1110

--- readfromFile.py
+++ readfromFile.py
@@ -1,4 +1,6 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 from cryptoHash import sha256
 
 def readData_fromFile(nameOftheFile):
@@ -47,6 +49,106 @@ for data in dataList:
 
     outList.append(hash)
 
+=======
+from cryptoHash import sha256
+
+def readData_fromFile(nameOftheFile):
+    myReadblefile = open("data.txt", "r")
+
+
+    dataList = myReadblefile.readlines()
+
+#closing the file
+    myReadblefile.close()
+#itterate through list
+    for line in dataList:
+    #printing 
+        print(line)
+
+
+
+def writeData_toFile(ouputFileName, dataList):
+    myWritableFile = open('output.txt', 'w')
+
+#ittrearting through the list
+    for line in dataList:
+    #writing a string to a file
+        myWritableFile.write(line)
+
+#writing multiple strings at atime
+    myWritableFile.writelines("Additional Data")
+
+
+
+#close
+    myWritableFile.close()
+
+
+
+
+dataList= readData_fromFile("data.txt")
+
+outList = []
+for data in dataList:
+    hash = sha256(data)
+    print(hash)
+
+    length = len(hash)
+    print(length)
+
+    outList.append(hash)
+
+>>>>>>> b6de8fe534019f087940b43791f09b2c609938c3
+=======
+from cryptoHash import sha256
+
+def readData_fromFile(nameOftheFile):
+    myReadblefile = open("data.txt", "r")
+
+
+    dataList = myReadblefile.readlines()
+
+#closing the file
+    myReadblefile.close()
+#itterate through list
+    for line in dataList:
+    #printing 
+        print(line)
+
+
+
+def writeData_toFile(ouputFileName, dataList):
+    myWritableFile = open('output.txt', 'w')
+
+#ittrearting through the list
+    for line in dataList:
+    #writing a string to a file
+        myWritableFile.write(line)
+
+#writing multiple strings at atime
+    myWritableFile.writelines("Additional Data")
+
+
+
+#close
+    myWritableFile.close()
+
+
+
+
+dataList= readData_fromFile("data.txt")
+
+outList = []
+for data in dataList:
+    hash = sha256(data)
+    print(hash)
+
+    length = len(hash)
+    print(length)
+
+    outList.append(hash)
+
+>>>>>>> b6de8fe534019f087940b43791f09b2c609938c3
 =======
 from cryptoHash import sha256
 
